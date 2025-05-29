e0a2d6f33e28ed4d794bf0262131d66ffa1634b6
RuslanRuslan89 <ruslankon89@gmail.com>
2025-05-29 21:22:03 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Простой скрипт для формы регистрации
+document.querySelector('.reg-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert("Спасибо! На ваш номер отправлено подтверждение.");
+});
